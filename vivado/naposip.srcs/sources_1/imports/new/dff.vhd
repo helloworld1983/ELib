fe7c051bd8eafc9b936a90c9df83510758a7d947
Botond Sandor Kirei <botond.kirei@bel.utcluj.ro>
2018-03-05 17:47:10 +0200

    Added structural description of DFF (D flip-flop) Dependencies: nand3_gate, latchD

--- vivado/naposip.srcs/sources_1/imports/new/dff.vhd
+++ vivado/naposip.srcs/sources_1/imports/new/dff.vhd
@@ -57,3 +57,45 @@ energy_1: energy_sum port map (sum_in => 0, sum_out => en1, energy_mon => D);
 energy_2: energy_sum port map (sum_in => en1, sum_out => en2, energy_mon => Ck);
 energy_3: energy_sum port map (sum_in => en2, sum_out => energy_mon, energy_mon => R);
 end Behavioral;
+
+architecture Structural of dff is
+
+component latchD is
+Generic ( active_front : boolean := true;
+            dff_delay : time := 1 ns);
+   Port ( D : in STD_LOGIC;
+          Ck : in STD_LOGIC;
+          R : in STD_LOGIC;
+          Q, Qn : out STD_LOGIC;
+          energy_mon : out integer);
+end component;
+
+component  delay_cell is
+    Generic (delay : time :=1 ns);
+    Port ( a : in STD_LOGIC;
+           y : out STD_LOGIC;
+           energy_mon: out integer);
+end component;
+
+signal net: STD_LOGIC_VECTOR (1 to 4);
+signal en1,en2,en3 : integer;
+
+begin
+
+inversor: delay_cell port map (a => Ck, y => net(1), energy_mon => en1);
+
+rising_active: if (active_front) generate
+                D1: latchD generic map (active_front => true) port map (D => D, Ck => net(1), R => R, Q => net(2), energy_mon => en2); 
+                D2: latchD generic map (active_front => true) port map (D => net(2), Ck => Ck, R => R, Q => net(3), Qn => net(4),energy_mon => en3);        
+end generate rising_active;
+
+falling_active: if (not active_front) generate
+                D1: latchD generic map (active_front => false) port map (D => D, Ck => net(1), R => R, Q => net(2), energy_mon => en2); 
+                D2: latchD generic map (active_front => false) port map (D => net(2), Ck => Ck, R => R, Q => net(3), Qn => net(4),energy_mon => en3);       
+end generate falling_active ;
+
+Q <= net(3);
+Qn <= net(4);
+energy_mon <= en1 + en2 + en3;
+
+end Structural;
