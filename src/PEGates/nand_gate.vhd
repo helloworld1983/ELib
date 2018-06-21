d08c91654fc289bc99b2d490eabc70171f2e7289
Botond Sandor Kirei <botond.kirei@bel.utcluj.ro>
2018-06-21 13:40:52 +0300

    update on extened paper

--- src/PEGates/nand_gate.vhd
+++ src/PEGates/nand_gate.vhd
@@ -15,8 +15,8 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-library xil_defaultlib;
-use xil_defaultlib.PElib.all;
+library work;
+use work.PElib.all;
 
 entity nand_gate is
     Generic (delay : time := 1 ns;
